d45dc700b058198dfc87fe907fa61a21e025faff
Fujiwara Tofu Shop <fujiwara@foundnone.xyz>
2026-02-17 17:03:20 -0800

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  base: './',
+  base: '/cortisol-dodge/',
   publicDir: 'public',
   server: { port: 3000, open: true },
   build: { outDir: 'dist' },
